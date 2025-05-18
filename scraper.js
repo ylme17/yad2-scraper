486a1161f410d89c3c9d39eb074aee45c1ad999a
Yechiel M <41611048+ylme17@users.noreply.github.com>
2025-05-18 11:17:23 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -62,6 +62,7 @@ const scrapeItemsAndExtractImgUrls = async (url) => {
             const lnkSrc = $productBlock.attr('href');
             const imgSrc = $productBlock.find('img').attr('src');
             if (imgSrc && lnkSrc) data.push({ 'img': imgSrc, 'lnk': new URL(lnkSrc, url).href });
+            console.log(`data = ${data}`);
         });
     } else if (type === types.CARS || type === types.NADLAN) {
         const $imageList = $feedItems.find(stages[type][1]);
@@ -72,6 +73,7 @@ const scrapeItemsAndExtractImgUrls = async (url) => {
             const linkEl = $linkList[i];
             const lnkSrc = $(linkEl).attr('href') || $(linkEl).find('a').attr('href');
             if (imgSrc && lnkSrc) data.push({ 'img': imgSrc, 'lnk': new URL(lnkSrc, url).href });
+            console.log(`data = ${data}`);
         });
     } else {
         throw new Error("Cannot scrape unknown type, selectors are not defined.");
